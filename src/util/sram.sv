2edf5c6a00a91e333ca5e61cfb84262a86551be2
Killian Storm <killianstorm@gmail.com>
2019-11-26 13:33:35 +0100

    Triple redundancy

--- src/util/sram.sv
+++ src/util/sram.sv
@@ -36,18 +36,84 @@ module sram #(
 localparam DATA_WIDTH_ALIGNED = ((DATA_WIDTH+63)/64)*64;
 localparam BE_WIDTH_ALIGNED   = (((DATA_WIDTH+7)/8+7)/8)*8;
 
+// Red1
 logic [DATA_WIDTH_ALIGNED-1:0]  wdata_aligned;
 logic [BE_WIDTH_ALIGNED-1:0]    be_aligned;
 logic [DATA_WIDTH_ALIGNED-1:0]  rdata_aligned;
 
+// Red2
+logic [DATA_WIDTH_ALIGNED-1:0]  wdata_aligned2;
+logic [BE_WIDTH_ALIGNED-1:0]    be_aligned2;
+logic [DATA_WIDTH_ALIGNED-1:0]  rdata_aligned2;
+
+// Red3
+logic [DATA_WIDTH_ALIGNED-1:0]  wdata_aligned3;
+logic [BE_WIDTH_ALIGNED-1:0]    be_aligned3;
+logic [DATA_WIDTH_ALIGNED-1:0]  rdata_aligned3;
+
+logic compare_1_2;
+logic compare_2_3;
+logic compare_1_3;
+
+
 // align to 64 bits for inferrable macro below
 always_comb begin : p_align
-    wdata_aligned                    ='0;
-    be_aligned                       ='0;
-    wdata_aligned[DATA_WIDTH-1:0]    = wdata_i;
-    be_aligned[BE_WIDTH_ALIGNED-1:0] = be_i;
+    // Red1
+    wdata_aligned                     ='0;
+    be_aligned                        ='0;
+    wdata_aligned[DATA_WIDTH-1:0]     = wdata_i;
+    be_aligned[BE_WIDTH_ALIGNED-1:0]  = be_i;
 
-    rdata_o = rdata_aligned[DATA_WIDTH-1:0];
+    assign compare_1_2 = (rdata_aligned[DATA_WIDTH-1:0] == rdata_aligned2[DATA_WIDTH-1:0]);
+    assign compare_2_3 = (rdata_aligned2[DATA_WIDTH-1:0] == rdata_aligned3[DATA_WIDTH-1:0]);
+    assign compare_1_3 = (rdata_aligned[DATA_WIDTH-1:0] == rdata_aligned3[DATA_WIDTH-1:0]);
+
+    if (compare_1_2 == 1'b1 && 
+        compare_2_3 == 1'b1 &&
+        compare_1_3 == 1'b1) 
+        begin
+            rdata_o = rdata_aligned[DATA_WIDTH-1:0];
+        end
+    else 
+        begin
+
+            // If comparison of 1 and 2 is 1, then sram 3 is faulty.
+            if (compare_1_2 == 1'b1)
+                begin
+                    rdata_o = rdata_aligned[DATA_WIDTH-1:0];
+                end
+            else 
+                begin
+                    // If comparison of 2 and 3 is 1, then sram 1 is faulty.
+                    if (compare_2_3 == 1'b1)
+                        begin
+                            rdata_o = rdata_aligned2[DATA_WIDTH-1:0];
+                        end
+                    else
+                        begin
+                            // If comparison of 1 and 3 is 1, then sram 2 is faulty.
+                            if (compare_1_3 == 1'b1)
+                            begin
+                                        rdata_o = rdata_aligned[DATA_WIDTH-1:0];
+                            end
+                        end
+                end
+
+        end
+
+
+
+    // Red2
+    wdata_aligned2                    ='0;
+    be_aligned2                       ='0;
+    wdata_aligned2[DATA_WIDTH-1:0]    = wdata_i;
+    be_aligned2[BE_WIDTH_ALIGNED-1:0] = be_i;
+
+    // Red3
+    wdata_aligned3                    ='0;
+    be_aligned3                       ='0;
+    wdata_aligned3[DATA_WIDTH-1:0]    = wdata_i;
+    be_aligned3[BE_WIDTH_ALIGNED-1:0] = be_i;
 end
 
 genvar k;
@@ -70,6 +136,54 @@ generate
            .WrData_DI ( wdata_aligned[k*64 +: 64] ),
            .Addr_DI   ( addr_i                    ),
            .RdData_DO ( rdata_aligned[k*64 +: 64] )
+        );
+    end
+endgenerate
+
+genvar kk;
+generate
+    for (kk = 0; kk<(DATA_WIDTH+63)/64; kk++) begin
+        // unused byte-enable segments (8bits) are culled by the tool
+        SyncSpRamBeNx64 #(
+          .ADDR_WIDTH($clog2(NUM_WORDS)),
+          .DATA_DEPTH(NUM_WORDS),
+          .OUT_REGS (0),
+          // this initializes the memory with 0es. adjust to taste...
+          // 0: no init, 1: zero init, 2: random init, 3: deadbeef init
+          .SIM_INIT (1)
+        ) i_ram (
+           .Clk_CI    ( clk_i                      ),
+           .Rst_RBI   ( rst_ni                     ),
+           .CSel_SI   ( req_i                      ),
+           .WrEn_SI   ( we_i                       ),
+           .BEn_SI    ( be_aligned2[kk*8 +: 8]      ),
+           .WrData_DI ( wdata_aligned2[kk*64 +: 64] ),
+           .Addr_DI   ( addr_i                     ),
+           .RdData_DO ( rdata_aligned2[kk*64 +: 64] )
+        );
+    end
+endgenerate
+
+genvar kkk;
+generate
+    for (kkk = 0; kkk<(DATA_WIDTH+63)/64; kkk++) begin
+        // unused byte-enable segments (8bits) are culled by the tool
+        SyncSpRamBeNx64 #(
+          .ADDR_WIDTH($clog2(NUM_WORDS)),
+          .DATA_DEPTH(NUM_WORDS),
+          .OUT_REGS (0),
+          // this initializes the memory with 0es. adjust to taste...
+          // 0: no init, 1: zero init, 2: random init, 3: deadbeef init
+          .SIM_INIT (1)
+        ) i_ram (
+           .Clk_CI    ( clk_i                      ),
+           .Rst_RBI   ( rst_ni                     ),
+           .CSel_SI   ( req_i                      ),
+           .WrEn_SI   ( we_i                       ),
+           .BEn_SI    ( be_aligned3[kkk*8 +: 8]      ),
+           .WrData_DI ( wdata_aligned3[kkk*64 +: 64] ),
+           .Addr_DI   ( addr_i                     ),
+           .RdData_DO ( rdata_aligned3[kkk*64 +: 64] )
         );
     end
 endgenerate
